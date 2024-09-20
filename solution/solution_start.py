16d13aa0289f2810279a009e15f7e0cd2135a2df
Peter Woodcock <peter.woodcock@digital.trade.gov.uk>
2024-09-20 11:26:47 +0100

    WIP: add duckdb and stop using data generator

--- solution/solution_start.py
+++ solution/solution_start.py
@@ -1,46 +1,32 @@
 import argparse
-import glob
+import duckdb
 
-import pandas.io.parsers
+ADVISORS_FILE_LOCATION = "./input_data/advisors.csv"
+COMMODITIES_FILE_LOCATION = "./input_data/commodities.csv"
+COUNTRIES_FILE_LOCATION = "./input_data/countries.csv"
+EXPORT_WINS_FILE_LOCATION = "./input_data/export_wins.csv"
 
-
-def read_csv(csv_location: str):
-    return pandas.read_csv(csv_location, header=0)
-
-
-def read_json_folder(json_folder: str):
-    transactions_files = glob.glob("{}*/*.json".format(json_folder))
-
-    return pandas.concat(pandas.read_json(tf, lines=True) for tf in transactions_files)
+OUTPUT_DIRECTORY = "./output/most_valuable_export_wins.csv"
 
 
-def run_transformations(customers_location: str, products_location: str,
-                        transactions_location: str, output_location: str):
-    customers_df = read_csv(customers_location)
-    products_df = read_csv(products_location)
-    transactions_df = read_json_folder(transactions_location)
+def load_and_query_data(top: int):
+    advisors = duckdb.read_csv(ADVISORS_FILE_LOCATION)
+    commodities = duckdb.read_csv(COMMODITIES_FILE_LOCATION)
+    countries = duckdb.read_csv(COUNTRIES_FILE_LOCATION)
+    export_wins = duckdb.read_csv(EXPORT_WINS_FILE_LOCATION)
 
-    return get_latest_transaction_date(transactions_df)
-
-
-def get_latest_transaction_date(transactions):
-    latest_purchase = transactions.date_of_purchase.max()
-    latest_transaction = transactions[transactions.date_of_purchase == latest_purchase]
-    return latest_transaction
+    get_most_valuable_export_wins(export_wins)
 
 
-def to_canonical_date_str(date_to_transform):
-    return date_to_transform.strftime('%Y-%m-%d')
+def get_most_valuable_export_wins(export_wins):
+    sql = "SELECT * FROM export_wins"
+    return duckdb.sql(sql).show()
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='DataTest')
-    parser.add_argument('--customers_location', required=False, default="./input_data/starter/customers.csv")
-    parser.add_argument('--products_location', required=False, default="./input_data/starter/products.csv")
-    parser.add_argument('--transactions_location', required=False, default="./input_data/starter/transactions/")
-    parser.add_argument('--output_location', required=False, default="./output_data/outputs/")
+    parser.add_argument('--top', required=False, default=3)
     args = vars(parser.parse_args())
 
-    run_transformations(args['customers_location'], args['products_location'],
-                        args['transactions_location'], args['output_location'])
+    load_and_query_data(args['top'])
